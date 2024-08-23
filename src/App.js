b8e8f45d66007abcb194bd165d4cb519b92f05d7
Ajith <56928441+ajithreddy55@users.noreply.github.com>
2024-08-22 21:06:44 -0500

    adding files

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 import { useState } from 'react';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Routes,
-  BrowserRouter
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   Routes,
+//   BrowserRouter
+// } from "react-router-dom"; because routing doesnt work properly with the github deployment.
 
 
-let name = "Ajith";
+// let name = "Ajith";
 
 function App() {
 
@@ -52,26 +52,26 @@ function App() {
   return (
  <>
 
- <BrowserRouter>
+ {/* <BrowserRouter> */}
 <Navbar title="TextUtils" About="About Utils" mode={mode} toggleMode={toggleMode} />
 
 <Alert alert ={alert}/>
 
 <div className="container">
-<Routes>
+{/* <Routes>
           <Route exact path="/about" element={<About />}/>
             {/* <About /> */}
           {/* </Route> */}
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />}/>
+          {/* <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />}/> */}
           
           {/* <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} /> */}
           {/* </Route> */}
-</Routes>
+{/* </Routes> */} 
 
-{/* <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} /> */}
+<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />
 
 </div>
-</BrowserRouter>
+{/* </BrowserRouter> */}
 
 
 {/* <About/> */}
